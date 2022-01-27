3bf3e43acaaa30296eb309a2bc833e8aeed2c9af
Malto4 <30811936+Malto4@users.noreply.github.com>
2022-01-27 13:20:28 +0100

    Update main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/Malto4/reporter/grafana"
+	"github.com/Malto4/reporter/report"
 	"github.com/gorilla/mux"
 )
 
